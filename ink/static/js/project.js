435ea2712347c3756d4d61738cbe0eb4df8f0c09
nick <nick@inkpressure.com>
2019-04-06 16:42:17 -0400

    Cookiecutter Base Configuration Custom Bootstrap Navbar Customized Background Customized Spray Audio Logo Animation iro.js ColorPicker adjustments on checkboxes for custom paint choices Whoa! Font Added as static url Shopping Page Created in place of About Page Color Picker Swatches

--- ink/static/js/project.js
+++ ink/static/js/project.js
@@ -1,1 +1,8 @@
 /* Project specific Javascript goes here. */
+/*!
+ * iro-dynamic-css v1.0.4
+ * iro.js plugin to dynamically update CSS rules whenever the selected color changes
+ * 2019 James Daniel
+ * Licensed under MPL 2.0
+ * github.com/jaames/iro-dynamic-css
+ */
