e141ed35b40ee243f9f80f84560b7313acdfd06f
Matt Barry <matt@hazelmollusk.org>
2019-04-23 22:16:25 -0400

    .

---
+++ squeeze/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from oscar.app import application
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
+
+urlpatterns = []
